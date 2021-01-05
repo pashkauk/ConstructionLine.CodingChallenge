19b18e605adcbaaf9b13f70ab728c4eda944d846
Pavels Pantelejevs <pavels.pantelejevs@sembcorp.com>
2021-01-05 19:03:59 +0000

    Init

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+        [Test]
+        public void Test()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red},
+                Sizes = new List<Size> {Size.Small}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void Proof()
+        {
+            //This test designed especially to proof that requrements are not mach the test
+            //This set up as per readme requrements and should pass extra asserts if tests and requrements are inline
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Blue - Medium", Size.Medium, Color.Blue)
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red },
+                Sizes = new List<Size> { Size.Small, Size.Medium }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+            //here we clearly can see that all previouse tests are passed
+
+
+            //Shirts = List<Shirt> { SmallRedShirt },
+            //SizeCounts = List<SizeCount> { Small(1), Medium(0), Large(0)},
+            //ColorCounts = List<ColorCount> { Red(1), Blue(0), Yellow(0), White(0), Black(0)}
+
+            var blueCount = results.ColorCounts.First(x => x.Color == Color.Blue).Count;
+            //And this assert will be failing, however requrements stating it shoulb be 0
+  
+            Assert.That(blueCount, Is.EqualTo(0), $"Blue color count shoulbe 0 according requrements");
+        }
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+        }
+
+
+        public SearchResults Search(SearchOptions options)
+        {
+            var result = _shirts.Where(x => options.Sizes.Any(s => s.Id == x.Size.Id) && options.Colors.Any(c => c.Id == x.Color.Id)).ToList();
+
+            var colors = new List<ColorCount>();
+            var sizes = new List<SizeCount>();
+
+            foreach (var color in Color.All)
+            {
+                colors.Add(new ColorCount
+                {
+                    Color = color,
+                    Count = _shirts.Count(c => c.Color.Id == color.Id
+                      && (!options.Sizes.Any() || options.Sizes.Select(s => s.Id).Contains(c.Size.Id)))
+                });
+            };
+
+            foreach (var size in Size.All)
+            {
+                sizes.Add(new SizeCount
+                {
+                    Size = size,
+                    Count = _shirts.Count(s => s.Size.Id == size.Id
+                                && (!options.Colors.Any() || options.Colors.Select(c => c.Id).Contains(s.Color.Id)))
+                });
+            };
+
+            var searchResults = new SearchResults
+            {
+                Shirts = result,
+                ColorCounts = colors,
+                SizeCounts = sizes
+            };
+
+            return searchResults;
+        }
+    }
+}
